06a70ff030bb2c70398fd397c62bfd8b92b600f1
pressxtohonk <pressxtohonk@gmail.com>
2026-02-23 03:09:18 +0800

    added openspec to devshell

--- flake.nix
+++ flake.nix
@@ -1,13 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    openspec.url = "github:Fission-AI/OpenSpec";
+    openspec.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... }: {
+  outputs = { nixpkgs, openspec, ... }: {
     devShells = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
       {
         default = nixpkgs.legacyPackages.${system}.mkShell {
           packages = [
+            openspec.packages.${system}.default
           ];
         };
       }
